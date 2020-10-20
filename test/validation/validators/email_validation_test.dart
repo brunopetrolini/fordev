0d483d80d963d364c2260beb65d0980b33f7103c
Bruno Petrolini <brunopetrolini@hotmail.com>
2020-10-20 16:29:27 -0300

    move duplicated code to the setup method

--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:test/test.dart';
 
 import 'package:for_dev/validation/protocols/field_validation.dart';
@@ -15,15 +13,17 @@ class EmailValidation implements FieldValidation {
 }
 
 void main() {
-  test('Should return null if email is empty', () {
-    final sut = EmailValidation('any_field');
+  EmailValidation sut;
 
+  setUp(() {
+    sut = EmailValidation('any_field');
+  });
+
+  test('Should return null if email is empty', () {
     expect(sut.validate(''), null);
   });
 
   test('Should return null if email is null', () {
-    final sut = EmailValidation('any_field');
-
     expect(sut.validate(null), null);
   });
 }
