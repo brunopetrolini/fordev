112c44f3dabe18b9c4443893debb1ef532fa944c
Bruno Petrolini <brunopetrolini@hotmail.com>
2020-10-24 11:10:30 -0300

    create LoginPage factory

---
+++ lib/ui/components/app_theme.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+ThemeData makeAppTheme() {
+  final kPrimaryColor = Color.fromRGBO(136, 14, 79, 1);
+  final kPrimaryColorDark = Color.fromRGBO(96, 0, 39, 1);
+  final kPrimaryColorLight = Color.fromRGBO(188, 71, 123, 1);
+
+  return ThemeData(
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+    primaryColor: kPrimaryColor,
+    primaryColorDark: kPrimaryColorDark,
+    primaryColorLight: kPrimaryColorLight,
+    accentColor: kPrimaryColor,
+    backgroundColor: Colors.white,
+    textTheme: TextTheme(
+      headline1: TextStyle(
+        fontSize: 30,
+        fontWeight: FontWeight.bold,
+        color: kPrimaryColorDark,
+      ),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      enabledBorder: UnderlineInputBorder(
+        borderSide: BorderSide(color: kPrimaryColorLight),
+      ),
+      focusedBorder: UnderlineInputBorder(
+        borderSide: BorderSide(color: kPrimaryColor),
+      ),
+      alignLabelWithHint: true,
+    ),
+    buttonTheme: ButtonThemeData(
+      colorScheme: ColorScheme.light(primary: kPrimaryColor),
+      buttonColor: kPrimaryColor,
+      splashColor: kPrimaryColorLight,
+      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      textTheme: ButtonTextTheme.primary,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+    ),
+  );
+}
--- lib/ui/components/components.dart
+++ lib/ui/components/components.dart
@@ -1,4 +1,4 @@
-export './app.dart';
 export './spinner_dialog.dart';
 export './error_snackbar.dart';
 export './headline1.dart';
+export './app_theme.dart';
