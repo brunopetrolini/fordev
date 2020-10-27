ab18e9b83ed7305c7dbf3c7462c4a81da9a1cc21
Bruno Petrolini <brunopetrolini@hotmail.com>
2020-10-27 16:50:04 -0300

    remove hide loading in success case

--- lib/presentation/presenters/getx_login_presenter.dart
+++ lib/presentation/presenters/getx_login_presenter.dart
@@ -65,9 +65,8 @@ class GetxLoginPresenter extends GetxController implements LoginPresenter {
       await saveCurrentAccout.save(account);
     } on DomainError catch (error) {
       _mainError.value = error.description;
+      _isLoading.value = false;
     }
-
-    _isLoading.value = false;
   }
 
   void dispose() {}
