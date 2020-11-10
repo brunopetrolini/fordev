74bb283a64e8af1a38515b8ccb306a973a9e72ae
Bruno Petrolini <brunopetrolini@hotmail.com>
2020-11-10 12:03:24 -0300

    move RemoteLoadSurveys to its own file

--- test/data/usecases/load_surveys/remote_load_surveys_test.dart
+++ test/data/usecases/load_surveys/remote_load_surveys_test.dart
@@ -1,33 +1,12 @@
 import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
-import 'package:meta/meta.dart';
 
 import 'package:for_dev/domain/helpers/helpers.dart';
 import 'package:for_dev/domain/entities/entities.dart';
 
+import 'package:for_dev/data/usecases/usecases.dart';
 import 'package:for_dev/data/http/http.dart';
-import 'package:for_dev/data/models/models.dart';
-
-class RemoteLoadSurveys {
-  final HttpClient<List<Map>> httpClient;
-  final String url;
-
-  RemoteLoadSurveys({@required this.httpClient, @required this.url});
-
-  Future<List<SurveyEntity>> load() async {
-    try {
-      final response = await httpClient.request(url: url, method: 'get');
-      return response
-          .map((json) => RemoteSurveyModel.fromJson(json).toEntity())
-          .toList();
-    } on HttpError catch (error) {
-      throw error == HttpError.forbidden
-          ? DomainError.accessDenied
-          : DomainError.unexpected;
-    }
-  }
-}
 
 class HttpClientSpy extends Mock implements HttpClient<List<Map>> {}
 
