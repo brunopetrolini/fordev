9df9718a8ad6fa677577e189ff7fbde9f2df20ce
Bruno Petrolini <brunopetrolini@hotmail.com>
2020-11-11 21:47:52 -0300

    move GetxSurveysPresenter to its own file

--- test/presentation/presenters/getx_surveys_presenter_test.dart
+++ test/presentation/presenters/getx_surveys_presenter_test.dart
@@ -1,9 +1,6 @@
 import 'package:faker/faker.dart';
-import 'package:intl/intl.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
-import 'package:meta/meta.dart';
-import 'package:get/get.dart';
 
 import 'package:for_dev/domain/helpers/helpers.dart';
 import 'package:for_dev/domain/entities/entities.dart';
@@ -11,38 +8,7 @@ import 'package:for_dev/domain/usecases/usecases.dart';
 
 import 'package:for_dev/ui/pages/pages.dart';
 
-class GetxSurveysPresenter {
-  final LoadSurveys loadSurveys;
-
-  final _isLoading = true.obs;
-  final _surveys = Rx<List<SurveyViewModel>>();
-
-  Stream<bool> get isLoadingStream => _isLoading.stream;
-  Stream<List<SurveyViewModel>> get surveysStream => _surveys.stream;
-
-  GetxSurveysPresenter({@required this.loadSurveys});
-
-  Future<void> loadData() async {
-    try {
-      _isLoading.value = true;
-      final surveys = await loadSurveys.load();
-      _surveys.value = surveys
-          .map(
-            (survey) => SurveyViewModel(
-                id: survey.id,
-                question: survey.question,
-                date: DateFormat('dd MMM yyyy').format(survey.date),
-                didAnswer: survey.didAnswer),
-          )
-          .toList();
-    } on DomainError {
-      _surveys.subject
-          .addError('Algo errado aconteceu. Tente novamente em breve');
-    } finally {
-      _isLoading.value = false;
-    }
-  }
-}
+import 'package:for_dev/presentation/presenters/presenters.dart';
 
 class LoadSurveysSpy extends Mock implements LoadSurveys {}
 
