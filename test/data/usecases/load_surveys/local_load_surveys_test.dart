0f967907cdf08733809ed1e8bbf20c844e965096
Bruno Petrolini <brunopetrolini@hotmail.com>
2020-11-16 11:03:59 -0300

    ensure LocalLoadSurveys calls FetchCacheStorage with correct key

---
+++ test/data/usecases/load_surveys/local_load_surveys_test.dart
@@ -0,0 +1,30 @@
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+import 'package:meta/meta.dart';
+
+class LoalLoadSurveys {
+  final FetchCacheStorage fetchCacheStorage;
+
+  LoalLoadSurveys({@required this.fetchCacheStorage});
+
+  Future<void> load() async {
+    await fetchCacheStorage.fetch('surveys');
+  }
+}
+
+class FetchCacheStorageSpy extends Mock implements FetchCacheStorage {}
+
+abstract class FetchCacheStorage {
+  Future<void> fetch(String key);
+}
+
+void main() {
+  test('Should call FetchCacheStorage with correct key', () async {
+    final fetchCacheStorage = FetchCacheStorageSpy();
+    final sut = LoalLoadSurveys(fetchCacheStorage: fetchCacheStorage);
+
+    await sut.load();
+
+    verify(fetchCacheStorage.fetch('surveys')).called(1);
+  });
+}
