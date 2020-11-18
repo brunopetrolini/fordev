bb7e9437e7b5ffe8486e3448ef05ae4c5c8e933f
Bruno Petrolini <brunopetrolini@hotmail.com>
2020-11-18 09:57:26 -0300

    move LocalStorageAdapter to its own file

--- lib/infra/cache/local_storage_adapter.dart
+++ lib/infra/cache/local_storage_adapter.dart
@@ -1,20 +1,13 @@
+import 'package:localstorage/localstorage.dart';
 import 'package:meta/meta.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-import '../../data/cache/cache.dart';
+class LocalStorageAdapter {
+  final LocalStorage localStorage;
 
-class SecureStorageAdapter
-    implements SaveSecureCacheStorage, FetchSecureCacheStorage {
-  final FlutterSecureStorage secureStorage;
+  LocalStorageAdapter({@required this.localStorage});
 
-  SecureStorageAdapter({@required this.secureStorage});
-
-  Future<void> saveSecure(
-      {@required String key, @required String value}) async {
-    await secureStorage.write(key: key, value: value);
-  }
-
-  Future<String> fetchSecure(String key) async {
-    return await secureStorage.read(key: key);
+  Future<void> save({@required String key, @required dynamic value}) async {
+    await localStorage.deleteItem(key);
+    await localStorage.setItem(key, value);
   }
 }
