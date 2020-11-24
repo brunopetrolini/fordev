ec77247851aa4af45158df34236df995988fe05e
Bruno Petrolini <brunopetrolini@hotmail.com>
2020-11-24 20:03:15 -0300

    ensure SurveyResultPage calls LoadSurveyResult on page load

---
+++ test/ui/pages/surveys_result_page_test.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+
+import 'package:image_test_utils/image_test_utils.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:get/get.dart';
+
+import 'package:for_dev/ui/pages/pages.dart';
+
+class SurveyResultPresenterSpy extends Mock implements SurveyResultPresenter {}
+
+void main() {
+  SurveyResultPresenterSpy presenter;
+
+  Future<void> loadPage(WidgetTester tester) async {
+    presenter = SurveyResultPresenterSpy();
+    final surveysPage = GetMaterialApp(
+      initialRoute: '/surveys_result/any_survey_id',
+      getPages: [
+        GetPage(
+          name: '/surveys_result/:survey_id',
+          page: () => SurveyResultPage(presenter),
+        ),
+      ],
+    );
+    provideMockedNetworkImages(() async {
+      await tester.pumpWidget(surveysPage);
+    });
+  }
+
+  testWidgets('Should call LoadSurveyResult on page load',
+      (WidgetTester tester) async {
+    await loadPage(tester);
+
+    verify(presenter.loadData()).called(1);
+  });
+}
