f4665a4fafe7202d58633751382112b0f93219d7
Bruno Petrolini <brunopetrolini@hotmail.com>
2020-12-16 20:18:08 -0300

    move GetxSurveyResultPresenter to its own file

--- test/presentation/presenters/getx_survey_result_presenter_test.dart
+++ test/presentation/presenters/getx_survey_result_presenter_test.dart
@@ -1,8 +1,6 @@
 import 'package:faker/faker.dart';
-import 'package:get/get.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
-import 'package:meta/meta.dart';
 
 import 'package:for_dev/domain/helpers/helpers.dart';
 import 'package:for_dev/domain/entities/entities.dart';
@@ -10,46 +8,7 @@ import 'package:for_dev/domain/usecases/usecases.dart';
 
 import 'package:for_dev/ui/pages/pages.dart';
 
-class GetxSurveyResultPresenter extends GetxController
-    implements SurveyResultPresenter {
-  final LoadSurveyResult loadSurveyResult;
-  final String surveyId;
-
-  final _isLoading = true.obs;
-  final _surveyResult = Rx<SurveyResultViewModel>();
-
-  Stream<bool> get isLoadingStream => _isLoading.stream;
-  Stream<SurveyResultViewModel> get surveyResultStream => _surveyResult.stream;
-
-  GetxSurveyResultPresenter({@required this.loadSurveyResult, this.surveyId});
-
-  Future<void> loadData() async {
-    try {
-      _isLoading.value = true;
-
-      final surveyResult =
-          await loadSurveyResult.loadBySurvey(surveyId: surveyId);
-
-      _surveyResult.value = SurveyResultViewModel(
-        surveyId: surveyResult.surveyId,
-        question: surveyResult.question,
-        answers: surveyResult.answers
-            .map((answer) => SurveyAnswerViewModel(
-                  image: answer.image,
-                  answer: answer.answer,
-                  isCurrentAnswer: answer.isCurrentAnswer,
-                  percent: '${answer.percent}%',
-                ))
-            .toList(),
-      );
-    } on DomainError {
-      _surveyResult.subject
-          .addError('Algo errado aconteceu. Tente novamente em breve');
-    } finally {
-      _isLoading.value = false;
-    }
-  }
-}
+import 'package:for_dev/presentation/presenters/presenters.dart';
 
 class LoadSurveyResultSpy extends Mock implements LoadSurveyResult {}
 
