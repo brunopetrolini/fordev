e83ea4db079dab3c263aed0839874a5fc90113ac
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-01-11 21:18:32 -0300

    remove unused param

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -25,15 +25,16 @@ class HttpAdapter implements HttpClient {
 
     var response = Response('', 500);
 
+    Future<Response> futureResponse;
     try {
       if (method == 'post') {
-        response = await client
-            .post(url, headers: defaultHeaders, body: jsonBody)
-            .timeout(Duration(seconds: 10));
+        futureResponse =
+            client.post(url, headers: defaultHeaders, body: jsonBody);
       } else if (method == 'get') {
-        response = await client
-            .get(url, headers: defaultHeaders)
-            .timeout(Duration(seconds: 10));
+        futureResponse = client.get(url, headers: defaultHeaders);
+      }
+      if (futureResponse != null) {
+        response = await futureResponse.timeout(Duration(seconds: 5));
       }
     } catch (error) {
       throw HttpError.serverError;
--- test/main/composites/remote_load_survey_result_with_local_fallback_test.dart
+++ test/main/composites/remote_load_survey_result_with_local_fallback_test.dart
@@ -73,8 +73,7 @@ void main() {
   test('Shoul call local save with remote data', () async {
     await sut.loadBySurvey(surveyId: surveyId);
 
-    verify(local.save(surveyId: surveyId, surveyResult: remoteResult))
-        .called(1);
+    verify(local.save(remoteResult)).called(1);
   });
 
   test('Should return remote data', () async {
